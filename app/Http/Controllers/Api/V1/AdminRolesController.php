fee829a10ec4e68550db5c12d80d0bb173971b0e
mora <amarghandour89@gmail.com>
2022-08-16 15:11:13 +0200

    [add-feature] admin can add roles and assign roles to them.

---
+++ app/Http/Controllers/Api/V1/AdminRolesController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RoleResource;
+use App\Http\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Spatie\Permission\Models\Role;
+
+class AdminRolesController extends Controller
+{
+    use ApiResponse;
+
+    public function index()
+    {
+        $this->authorize('role_access');
+
+        $roles = Role::with('permissions')->paginate();
+
+        return $this->response(200, true, null, $roles);
+    }
+
+    public function show(Role $role)
+    {
+        $this->authorize('role_show');
+
+        return $this->response(200, true, null, RoleResource::make($role));
+    }
+
+    public function store(Request $request)
+    {
+        $this->authorize('role_create');
+
+        $request->validate([
+            'name' => ['required'],
+            'permissions' => ['required', Rule::exists('permissions', 'id')]
+        ]);
+
+        $role = Role::create([
+            'name' => $request->input('name'),
+            'guard_name' => 'api',
+        ]);
+
+        $role->syncPermissions($request->input('permissions'));
+
+        return $this->response(201, true, null, RoleResource::make($role), 'New role successfully created.');
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $this->authorize('role_edit');
+
+        $request->validate([
+            'name' => 'required',
+            'permissions' => ['required', Rule::exists('permissions', 'id')]
+        ]);
+
+        $role->update([
+            'name' => $request->input('name'),
+        ]);
+
+        $role->syncPermissions($request->input('permissions'));
+
+        return $this->response(201, true, null, RoleResource::make($role), 'Role successfully updated.');
+    }
+
+    public function destroy(Role $role)
+    {
+        $this->authorize('role_delete');
+
+        if ($role->name == 'Super Admin' || $role->name == 'User')
+            return $this->response(403, true, 'Forbidden', null, 'You cannot delete this role.');
+
+        $role->delete();
+
+        return $this->response(204);
+    }
+}
--- app/Http/Resources/PermissionResource.php
+++ app/Http/Resources/PermissionResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class RoleResource extends JsonResource
+class PermissionResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,8 +15,8 @@ class RoleResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name,
-//            'permissions' => PermissionResource::collection($this->whenLoaded('permissions')),
+            'id' => $this->id,
+            'name' => $this->name
         ];
     }
 }
